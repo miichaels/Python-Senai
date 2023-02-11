795f873922a40982817b975fe9c98cf056db9928
Michael Silva <88103166+miichaels@users.noreply.github.com>
2023-02-11 11:25:45 -0300

    Exercícios de funções

---
+++ PythonFuncoes.py
@@ -0,0 +1,48 @@
+        # Funções
+
+""" 
+input()
+range()
+type()
+float() 
+"""
+
+
+
+def prifunc():
+    print('Definindo com funções "def" em Python!!!');
+prifunc();
+prifunc();
+prifunc();
+prifunc();
+prifunc();
+
+def prifunc():
+    nome = 'Python';
+    print(f'Definindo com funções "def" em {nome} !!!');
+prifunc();
+prifunc();
+prifunc();
+prifunc();
+prifunc();
+
+
+def prifunc(nome):
+    #nome = 'Python';
+    print(f'Definindo com funções "def" em {nome}!!!');
+prifunc('Python');
+prifunc('Python');
+prifunc('Python');
+prifunc('Python');
+prifunc('Java');
+
+
+def num(p , n):
+    print("Primeiro número: " + str(p) + "!!!");
+    print("Segundo número: " + str(n) + "!!!");
+    print("Soma: " , p + n);
+num(4 , 8);
+num(15 , 45);
+num(47 , 18);
+num(4 , 87);
+num(4 , 65);
