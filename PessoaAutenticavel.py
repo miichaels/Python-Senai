7170cdf08b9f9a6997bd0c59a2ee8b8d302e4ae8
Michael Silva <88103166+miichaels@users.noreply.github.com>
2023-02-25 16:55:25 -0300

    Classe de autenticação com Herança

---
+++ PessoaAutenticavel.py
@@ -0,0 +1,24 @@
+class PessoaAut: #Classe Principal
+    def __init__(self, nome, sobrenome, idade):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome}'
+    
+    #----------
+    
+class PessoaAutenticavel(PessoaAut): #Classe filha
+    def __init__(self, nome, sobrenome, idade, usuario, senha):
+        PessoaAut.__init__(self, nome, sobrenome, idade)
+        self.usuario = usuario
+        self.senha = senha
+
+    def autenticar(self, usuario, senha):
+        return self.usuario == usuario and self.senha == senha
+    
+p = PessoaAutenticavel("Michael", "Silva", 25, "sp", "sabao")
+
+print(PessoaAut.nome_completo(p))
+print(PessoaAutenticavel.autenticar(p, "sp", "sabao"))
