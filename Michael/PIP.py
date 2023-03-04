346305f36debd533c51a4187e7e735c6e38e355a
Michael Silva <88103166+miichaels@users.noreply.github.com>
2023-03-04 15:58:30 -0300

    Delete Michael directory

--- Michael/PIP.py
+++
@@ -1,6 +0,0 @@
-import speedtest
-test = speedtest.Speedtest();
-down = test.download();
-up = test.upload();
-print(f'Taxa de Download: {down:.2f}')
-print(f'Tava de Upload: {up:.2f}')
--- Michael/origem/arquivo.py
+++
@@ -1,6 +0,0 @@
-arquivo = open(input('Nome do arquivo a ser editado:'), 'r');
-texto = arquivo.readlines();
-texto.append(input("Insira o texto:"));
-arquivo = open(input("Nome do arquivo a ser editado:"), "w");
-arquivo.writelines(texto);
-arquivo.close();
